30439024d3a2cc266e090586c23e79567896dfe3
madanpandey47 <zeusmadan97@gmail.com>
2025-06-08 10:00:53 +0545

    New Commit for notes

--- CPP Apna/copy.cpp
+++ CPP Apna/copy.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
-
 
 class Student{
     private:
@@ -11,9 +9,6 @@ class Student{
 
     // Shallow copy copies all member values from one object to another.
     // While Deep copy copies the member values but also makes copies of any dynamically allocated memory that the members point to.
-    // 
-
-
 
     public:
     Student(string name, float gpa) {
@@ -24,31 +19,38 @@ class Student{
         *gpaptr = gpa;
     }
 
+    // DEEP COPY EXAMPLE :
+
     Student (Student &obj) {
         name = obj.name; // Copy the name
         gpaptr = new float; // Allocate new memory for GPA pointer
         // This is a deep copy constructor
         // For shallow copy, we would just copy the pointer
+        *(gpaptr)= *obj.gpaptr;
     }
 
-    void getinfo() {
-        cout << " \nStudent's Information:" << endl;
-        cout << "Student's Name: " << name << endl;
-        cout << "Student's GPA: " << *gpaptr << endl;
+    void getinfo(int a) {
+        cout << " \nStudent "<< a << " Information: "<< endl;
+        cout << "Name: " << name << endl;
+        cout << "GPA: " << *gpaptr << endl;
     }
 
+    void setgpaptr(float gpa) {
+        *gpaptr = gpa;
+    }
 };
 
 
 int main(){ 
    
     Student s1("John Doe", 3.8);
-    s1.getinfo();
+    s1.getinfo(1);
     // Shallow copy
     // Student s2(s1); // This creates a shallow copy of s1 into s2
     Student s2 = s1; // This creates a shallow copy of s1 into s2
-    // *(s2.gpaptr) = 4.0; // This will change the GPA of s1 as well, because s2.gpaptr points to the same memory location as s1.gpaptr 
-    s2.getinfo();
+    s2.setgpaptr(4.0); // This only changes s2’s GPA
+    s2.getinfo(2);
+    s1.getinfo(1); // s1 remains unchanged — deep copy success
 
     return 0;
 }
